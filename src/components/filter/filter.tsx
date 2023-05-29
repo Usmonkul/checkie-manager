0a2ec8cbe426ffeb5d9af530f4b634f8d54b8c2e
Usmon <usmonhakimov1999@gmail.com>
2023-05-29 23:54:17 +0900

    add select input to a filter

--- src/components/filter/filter.tsx
+++ src/components/filter/filter.tsx
@@ -1,41 +1,60 @@
 import React, { useState } from "react";
 import { Formik, Form, Field } from "formik";
-const Filter = () => {
-  const [filterWord, setFilterWord] = useState(["Checked", "Recently"]);
+interface FilterProps {
+  type?: "selector";
+}
 
+const Filter = ({ type }: FilterProps) => {
+  const [filterWord, setFilterWord] = useState(["Checked", "Recently"]);
+  const [selectedOption, setSelectedOption] = useState("");
+  const handleOptionChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedOption(event?.target.value);
+  };
   return (
     <div className="bg-primary_white shadow-lg px-4 py-4 rounded-md flex items-center justify-between border-l-8 border-dark_blue">
       <span className="text-xl">Filter:</span>
-      <div className="flex items-center space-x-2">
-        {filterWord.map((item, index) => (
-          <FilterButton name={item} key={index} />
-        ))}
-        <Formik
-          initialValues={{ filterName: "" }}
-          onSubmit={(values, { setSubmitting }) => {
-            setTimeout(() => {
-              const newFilterWord = [...filterWord, values.filterName];
-              setFilterWord(newFilterWord);
-              setSubmitting(false);
-            }, 200);
-          }}
+      {type === "selector" ? (
+        <select
+          className="border-2 py-1 px-4 rounded-md"
+          value={selectedOption}
+          onChange={handleOptionChange}
         >
-          <Form className="flex items-center space-x-1">
-            <Field
-              type="text"
-              name="filterName"
-              placeholder="Enter filter"
-              className="flex items-center lg:w-[150px] rounded-l-3xl px-4 border-2 border-black text-black text-sm h-[33px] focus:outline focus:outline-green-300 "
-            />
-            <button
-              type="submit"
-              className="text-lg border-2 rounded-r-3xl py-0 px-5 border-black font-medium cursor-pointer hover:border-light_blue"
-            >
-              +Add
-            </button>
-          </Form>
-        </Formik>
-      </div>
+          <option value="">Select category</option>
+          <option value="large">Large Category</option>
+          <option value="middle">Middle Category</option>
+        </select>
+      ) : (
+        <div className="flex items-center space-x-2">
+          {filterWord.map((item, index) => (
+            <FilterButton name={item} key={index} />
+          ))}
+          <Formik
+            initialValues={{ filterName: "" }}
+            onSubmit={(values, { setSubmitting }) => {
+              setTimeout(() => {
+                const newFilterWord = [...filterWord, values.filterName];
+                setFilterWord(newFilterWord);
+                setSubmitting(false);
+              }, 200);
+            }}
+          >
+            <Form className="flex items-center space-x-1">
+              <Field
+                type="text"
+                name="filterName"
+                placeholder="Enter filter"
+                className="flex items-center lg:w-[150px] rounded-l-3xl px-4 border-2 border-black text-black text-sm h-[33px] focus:outline focus:outline-green-300 "
+              />
+              <button
+                type="submit"
+                className="text-lg border-2 rounded-r-3xl py-0 px-5 border-black font-medium cursor-pointer hover:border-light_blue"
+              >
+                +Add
+              </button>
+            </Form>
+          </Formik>
+        </div>
+      )}
     </div>
   );
 };

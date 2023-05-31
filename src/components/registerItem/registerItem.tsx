b7c80892f2e630128c79bffda3435d331daf2e26
Usmon <usmonhakimov1999@gmail.com>
2023-05-31 13:08:04 +0900

    add item and register item func

--- src/components/registerItem/registerItem.tsx
+++ src/components/registerItem/registerItem.tsx
@@ -1,7 +1,119 @@
-import React from "react";
+import { SecondaryHeader } from "@/utils/utils";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import { AiOutlineClose, AiOutlineArrowDown } from "react-icons/ai";
+import { useState } from "react";
+import ContentCom from "../contentCom/contentCom";
+const RegisterItem = ({
+  close,
+  toggleHandler,
+}: {
+  close: boolean;
+  toggleHandler: () => void;
+}) => {
+  return (
+    <div
+      className={`fixed top-0 left-0 z-50 ${
+        !close ? "hidden" : "flex"
+      } items-center justify-center w-full h-full bg-gray-300/90 overflow-hidden`}
+    >
+      <div className="relative min-w-[700px] flex flex-col bg-white shadow-xl px-8 py-5 rounded-md space-y-3 ">
+        <AiOutlineClose
+          onClick={toggleHandler}
+          className="absolute top-4 right-4 text-xl hover:text-red-700 cursor-pointer"
+        />
+        <div className="text-center pb-2">
+          <SecondaryHeader title="Resgister an Inspection Target" />
+        </div>
+        <Formik
+          initialValues={{
+            itemName: "",
+            largeCategory: "",
+            MiddleCategory: "",
+          }}
+          validationSchema={Yup.object({
+            itemName: Yup.string()
+              .max(15, "Must be 15 characters or less")
+              .required("Required"),
+          })}
+          onSubmit={(values, { setSubmitting, resetForm }) => {
+            setSubmitting(false);
+            resetForm({
+              values: {
+                itemName: "",
+                largeCategory: "",
+                MiddleCategory: "",
+              },
+            });
+            // setTimeout(() => {
+            //   alert(JSON.stringify(values, null, 2));
+            //   setSubmitting(false);
+            // }, 400);
+          }}
+        >
+          <Form className="flex flex-col space-y-3">
+            <label className="text-lg font-semibold" htmlFor="itemName">
+              Item Name
+            </label>
+            <Field
+              className="rounded-md px-4 py-2 border-2 border-slate-350"
+              placeholder="Enter an item name..."
+              name="itemName"
+              type="text"
+            />
+            <ErrorMessage name="itemName" />
+            {/*  */}
+            <label className="text-lg font-semibold" htmlFor="">
+              Select Large Category
+            </label>
+            <Field
+              name="largeCategory"
+              as="select"
+              className="rounded-md px-4 py-2 border-2 border-slate-350"
+            >
+              <option value="red">Lab</option>
+              <option value="green">W19</option>
+              <option value="blue">Finance Building</option>
+            </Field>
+            {/*  */}
+            <label className="text-lg font-semibold" htmlFor="">
+              Select Middle Category
+            </label>
+            <Field
+              name="middleCategory"
+              as="select"
+              className="rounded-md px-4 py-2 border-2 border-slate-350"
+            >
+              <option value="red">basic</option>
+              <option value="green">Toilet</option>
+              <option value="blue">102 room</option>
+            </Field>
+            {/*  */}
+            <label className="text-lg font-semibold" htmlFor="">
+              Content
+            </label>
+            <ContentCom inputType="textarea" />
 
-const RegisterItem = () => {
-  return <div>RegisterItem</div>;
+            {/*  */}
+            <div className="flex items-center justify-between pt-4 space-x-6">
+              <button
+                className="bg-gray-300 py-2 px-8 rounded-md text-black font-semibold min-w-[190px]"
+                type="reset"
+              >
+                Reset
+              </button>
+              <button
+                className="bg-light_blue py-2 px-8 rounded-md text-white font-semibold min-w-[190px]"
+                type="submit"
+              >
+                Register
+              </button>
+            </div>
+          </Form>
+        </Formik>
+      </div>
+    </div>
+  );
 };
 
 export default RegisterItem;
--- src/pages/item.tsx
+++ src/pages/item.tsx
@@ -1,9 +1,120 @@
-import React from 'react'
-
+import React from "react";
+import Head from "next/head";
+import { ButtonPrimary, PrimaryHeader } from "@/utils/utils";
+import { BiSearchAlt } from "react-icons/bi";
+import { Filter } from "@/components";
+import { MdDeleteOutline, MdOutlineModeEdit } from "react-icons/md";
+import { useState } from "react";
+import { RegisterItem } from "@/components";
+import { uuid } from "uuidv4";
+import { ItemDataProps } from "../../types/types";
 const Item = () => {
+  const [close, setClose] = useState(false);
+  const toggleHandler = () => {
+    setClose((prev) => !prev);
+  };
+  const [itemData, setItemData] = useState(ItemData);
+  const handleDelete = (dataId: string) => {
+    setItemData((current: ItemDataProps[]) =>
+      current.filter((card: ItemDataProps) => card.id != dataId)
+    );
+  };
   return (
-    <div>Item</div>
-  )
-}
+    <div className="text-black">
+      <Head>
+        <title>Inspection Item</title>
+      </Head>
+      <div className="flex flex-col py-5 px-5 space-y-4">
+        <RegisterItem close={close} toggleHandler={toggleHandler} />
+        <div className="flex items-center justify-between ">
+          <PrimaryHeader title="Inspection Item" />
+          <div onClick={toggleHandler}>
+            <ButtonPrimary title="Register Item" />
+          </div>
+        </div>
+        <Filter />
+        <div className="flex flex-col border-l-8 border-dark_blue shadow-lg rounded-md">
+          <div className="bg-primary_white  px-4 py-2  flex items-center justify-between ">
+            <h4 className="font-medium text-lg">
+              Inspection items:{" "}
+              <span className="ml-3 text-light_blue">{30}</span>
+            </h4>
+            <div className="flex items-center space-x-3">
+              <input
+                type="search"
+                name="search"
+                id="search"
+                placeholder="Search inspection target.."
+                className="flex items-center lg:w-[350px] rounded-md px-4 border outline-none text-black text-sm h-[33px] focus:outline focus:outline-green-300"
+              />
+              <div className="border border-transparent hover:border-light_blue hover:bg-gray-200/80 p-[3px] rounded-md">
+                <BiSearchAlt className="text-2xl cursor-pointer" />
+              </div>
+            </div>
+          </div>
+          <div className=" bg-gray-100/40 px-4 py-1 flex items-center justify-between">
+            <span className="text-lg font-medium min-w-[30px]">No</span>
+            <span className="text-lg font-medium min-w-[150px]">
+              Detail Item
+            </span>
+            <span className="text-lg font-medium min-w-[150px]">
+              Large Category
+            </span>
+            <span className="text-lg font-medium min-w-[150px]">
+              Middle Category
+            </span>
+            <span className="text-lg font-medium min-w-[150px]">Date</span>
+            <span className="text-lg font-medium min-w-[150px]">
+              Modified Date
+            </span>
+            <div className="flex items-center space-x-4 justify-center min-w-[100px]">
+              <span className="text-lg font-medium">Edit</span>
+              <span className="text-lg font-medium">Delete</span>
+            </div>
+          </div>
+        </div>
+        {/* Target Items */}
+        <div className="flex flex-col space-y-1">
+          {itemData.map((item, index) => {
+            return (
+              <div
+                key={item.id}
+                className="bg-primary_white  px-4 py-3  flex items-center justify-between border-l-8 border-transparent  shadow-lg rounded-md"
+              >
+                <span className="text-lg font-medium min-w-[30px]">
+                  {index + 1}
+                </span>
+                <span className="text-lg min-w-[150px]">{item.check_item}</span>
+                <span className="text-lg min-w-[150px]">{item.checkClass}</span>
+                <span className="text-lg min-w-[150px]">
+                  {item.checkSubClass}
+                </span>
+                <span className="text-lg min-w-[150px]">{item.create_dt}</span>
+                <span className="text-lg min-w-[150px]">{item.update_dt}</span>
+                <div className="flex items-center space-x-7 text-2xl min-w-[100px]">
+                  <MdOutlineModeEdit className="text-dark_blue hover:text-lightest_blue" />
+                  <MdDeleteOutline
+                    onClick={() => handleDelete(item.id)}
+                    className="text-dark_blue hover:text-red-500"
+                  />
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Item
+export default Item;
+export const ItemData = [
+  {
+    id: uuid(),
+    check_item: "Power Pressure check",
+    checkClass: "large",
+    checkSubClass: "Basic",
+    create_dt: "2011.03.09",
+    update_dt: "2011.03.10",
+  },
+];

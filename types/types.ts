906e1df39052dae20854a03339cf54c6779f1720
Usmon <usmonhakimov1999@gmail.com>
2023-06-09 21:18:49 +0900

    add types

--- types/types.ts
+++ types/types.ts
@@ -40,3 +40,91 @@ export interface TargetDataProps {
   create_dt: string;
   update_dt: string;
 }
+
+//
+//
+export interface InspectorProps {
+  idx: number;
+  inspector_name: string;
+  inspector_tel: string;
+  inspector_email: string;
+  inspector_description: string;
+  inspector_image: string;
+  del_yn: string;
+  create_by: string;
+  create_dt: string;
+  update_by: string;
+  update_dt: string;
+}
+
+export interface TargetProps {
+  idx: number;
+  check_group: string;
+  create_by: string;
+  create_dt: string;
+  update_by: string;
+  update_dt: string;
+  file_name: string;
+  file_path: string;
+  file_size: string;
+  check_item_idx: 0;
+  tag_id: null;
+}
+
+export interface ItemProps {
+  idx: number;
+  check_class_idx: string;
+  checkClass: {
+    idx: number;
+    check_class: string;
+    del_yn: string;
+    create_by: string;
+    create_dt: string;
+    update_by: string;
+    update_dt: string;
+  };
+  check_sub_class_idx: string;
+  checkSubClass: {
+    idx: number;
+    check_sub_class: string;
+    del_yn: string;
+    create_by: string;
+    create_dt: string;
+    update_by: string;
+    update_dt: string;
+  };
+  check_item: string;
+  check_content: string;
+  del_yn: string;
+  create_by: string;
+  create_dt: string;
+  update_by: string;
+  update_dt: string;
+  check_item_type: string;
+}
+
+export interface ContentProps {
+  idx: number;
+  check_item: string;
+  check_content: string;
+}
+
+export interface LCategoryProps {
+  idx: number;
+  check_class: string;
+  del_yn: string;
+  create_by: string;
+  create_dt: string;
+  update_by: string;
+  update_dt: string;
+}
+
+export interface MCategoryProps {
+  idx: number;
+  check_sub_class: string;
+  del_yn: string;
+  create_by: string;
+  create_dt: string;
+  update_by: string;
+  update_dt: string;
+}

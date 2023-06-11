eea0b1c988809a6a178129939b9104c51fc24d8d
Usmon <usmonhakimov1999@gmail.com>
2023-06-11 11:49:16 +0900

    fix types

--- types/types.ts
+++ types/types.ts
@@ -1,46 +1,3 @@
-export interface CategoryProps {
-  id: string;
-  name: string;
-  category: string;
-  registeredDate: string;
-}
-
-export interface InspectorsProps {
-  id: string;
-  name: string;
-  job: string;
-  email: string;
-  phone: string;
-  description?: string;
-  image: string;
-}
-
-export interface DraftDataProps {
-  id: string;
-  title: string;
-  inspector: string;
-  inspectionDate: string;
-  registeredDate: string;
-  progress: string;
-  complationDate: string;
-}
-
-export interface ItemDataProps {
-  id: string;
-  check_item: string;
-  checkClass: string;
-  checkSubClass: string;
-  create_dt: string;
-  update_dt: string;
-}
-
-export interface TargetDataProps {
-  id: string;
-  facility: string;
-  create_dt: string;
-  update_dt: string;
-}
-
 //
 //
 export interface InspectorProps {
@@ -126,5 +83,49 @@ export interface MCategoryProps {
   create_by: string;
   create_dt: string;
   update_by: string;
+  update_dt: string;
+}
+
+// For test
+export interface CategoryProps {
+  id: string;
+  name: string;
+  category: string;
+  registeredDate: string;
+}
+
+export interface InspectorsProps {
+  id: string;
+  name: string;
+  job: string;
+  email: string;
+  phone: string;
+  description?: string;
+  image: string;
+}
+
+export interface DraftDataProps {
+  id: string;
+  title: string;
+  inspector: string;
+  inspectionDate: string;
+  registeredDate: string;
+  progress: string;
+  complationDate: string;
+}
+
+export interface ItemDataProps {
+  id: string;
+  check_item: string;
+  checkClass: string;
+  checkSubClass: string;
+  create_dt: string;
+  update_dt: string;
+}
+
+export interface TargetDataProps {
+  id: string;
+  facility: string;
+  create_dt: string;
   update_dt: string;
 }

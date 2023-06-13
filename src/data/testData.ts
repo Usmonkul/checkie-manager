25d2b7378e4bb90f684d23f474ebd036fd299f8b
Usmon <usmonhakimov1999@gmail.com>
2023-06-13 13:20:34 +0900

    add Test Data

---
+++ src/data/testData.ts
@@ -0,0 +1,219 @@
+import { v4 as uuid_v4 } from "uuid";
+
+export const InspectorExData = [
+  {
+    id: uuid_v4(),
+    name: "Donalt Trump",
+    job: "Inspector in Tech Lab",
+    email: "trump@gmail.com",
+    phone: "0107777777",
+    description: "10 years of experience in his field",
+    image: "https://placehold.co/600x400",
+  },
+  {
+    id: uuid_v4(),
+    name: "Donalt Trump",
+    job: "Inspector in Tech Lab",
+    email: "trump@gmail.com",
+    phone: "0107777777",
+    description: "10 years of experience in his field",
+    image: "https://placehold.co/600x400",
+  },
+  {
+    id: uuid_v4(),
+    name: "Donalt Trump",
+    job: "Inspector in Tech Lab",
+    email: "trump@gmail.com",
+    phone: "0107777777",
+    description: "10 years of experience in his field",
+    image: "https://placehold.co/600x400",
+  },
+  {
+    id: uuid_v4(),
+    name: "Donalt Trump",
+    job: "Inspector in Tech Lab",
+    email: "trump@gmail.com",
+    phone: "0107777777",
+    description: "10 years of experience in his field",
+    image: "https://placehold.co/600x400",
+  },
+  {
+    id: uuid_v4(),
+    name: "Donalt Trump",
+    job: "Inspector in Tech Lab",
+    email: "trump@gmail.com",
+    phone: "0107777777",
+    description: "10 years of experience in his field",
+    image: "https://placehold.co/600x400",
+  },
+  {
+    id: uuid_v4(),
+    name: "Donalt Trump",
+    job: "Inspector in Tech Lab",
+    email: "trump@gmail.com",
+    phone: "0107777777",
+    description: "10 years of experience in his field",
+    image: "https://placehold.co/600x400",
+  },
+];
+
+//Data for experiment
+export const ItemExData = [
+  {
+    id: uuid_v4(),
+    check_item: "Power Pressure check",
+    checkClass: "large",
+    checkSubClass: "Basic",
+    create_dt: "2011.03.09",
+    update_dt: "2011.03.10",
+  },
+  {
+    id: uuid_v4(),
+    check_item: "Power Pressure check",
+    checkClass: "large",
+    checkSubClass: "Basic",
+    create_dt: "2011.03.09",
+    update_dt: "2011.03.10",
+  },
+];
+
+//Data for exerperiment
+export const TargetExData = [
+  {
+    id: uuid_v4(),
+    facility: "Fite Station",
+    create_dt: "2011.03.09",
+    update_dt: "2011.03.10",
+  },
+  {
+    id: uuid_v4(),
+    facility: "Office",
+    create_dt: "2011.03.09",
+    update_dt: "2011.03.10",
+  },
+  {
+    id: uuid_v4(),
+    facility: "Plane Pruduction",
+    create_dt: "2011.03.09",
+    update_dt: "2011.03.10",
+  },
+  {
+    id: uuid_v4(),
+    facility: "Fite Station",
+    create_dt: "2011.03.09",
+    update_dt: "2011.03.10",
+  },
+];
+
+//History data
+export const HistoryExData = [
+  {
+    id: uuid_v4(),
+    name: "Fire Inspection Zone",
+    inspector: "Donalt J Trump",
+    date: "2011.03.09",
+  },
+  {
+    id: uuid_v4(),
+    name: "Water Inspection Zone",
+    inspector: "Donalt J Trump",
+    date: "2011.03.09",
+  },
+  {
+    id: uuid_v4(),
+    name: "Lab Inspection",
+    inspector: "Donalt J Trump",
+    date: "2011.03.09",
+  },
+  {
+    id: uuid_v4(),
+    name: "Water Inspection Zone",
+    inspector: "Donalt J Trump",
+    date: "2011.03.09",
+  },
+  {
+    id: uuid_v4(),
+    name: "Fire Inspection Zone",
+    inspector: "Donalt J Trump",
+    date: "2011.03.09",
+  },
+  {
+    id: uuid_v4(),
+    name: "Water Inspection Zone",
+    inspector: "Donalt J Trump",
+    date: "2011.03.09",
+  },
+  {
+    id: uuid_v4(),
+    name: "Fire Inspection Zone",
+    inspector: "Donalt J Trump",
+    date: "2011.03.09",
+  },
+];
+///Draft
+export const DraftExData = [
+  {
+    id: uuid_v4(),
+    title: "Fire Inspection Zone",
+    inspector: "Lianardo ",
+    inspectionDate: "2011.03.09",
+    registeredDate: "2010.04.03",
+    progress: "Success",
+    complationDate: "2010.04.03",
+  },
+  {
+    id: uuid_v4(),
+    title: "Water Inspection Zone",
+    inspector: "Donalt J Trump",
+    inspectionDate: "2011.03.09",
+    registeredDate: "2010.04.03",
+    progress: "Success",
+    complationDate: "2010.04.03",
+  },
+  {
+    id: uuid_v4(),
+    title: "Fire Inspection Zone",
+    inspector: "Lianardo ",
+    inspectionDate: "2011.03.09",
+    registeredDate: "2010.04.03",
+    progress: "Success",
+    complationDate: "2010.04.03",
+  },
+  {
+    id: uuid_v4(),
+    title: "Water Inspection Zone",
+    inspector: "Donalt J Trump",
+    inspectionDate: "2011.03.09",
+    registeredDate: "2010.04.03",
+    progress: "Success",
+    complationDate: "2010.04.03",
+  },
+];
+
+//Category
+export const CategoryExData = [
+  {
+    id: uuid_v4(),
+    name: "Category1",
+    category: "large",
+    registeredDate: "2011.03.09",
+  },
+  {
+    id: uuid_v4(),
+    name: "Category2",
+    category: "middle",
+    registeredDate: "2011.03.09",
+  },
+  {
+    id: uuid_v4(),
+    name: "Category3",
+    category: "large",
+    registeredDate: "2011.03.09",
+  },
+  {
+    id: uuid_v4(),
+    name: "Category4",
+    category: "middle",
+    registeredDate: "2011.03.09",
+  },
+];
